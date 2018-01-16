d8ec40f35532890641b4a3f2f14af664c0e13537
amaurel <alexandre.maurel@gmail.com>
2018-01-16 10:22:24 +0100

    init

---
+++ main.R
@@ -0,0 +1,25 @@
+library(tercen)
+library(dplyr)
+
+do.ttest = function(df, ...){
+  pv = NaN
+  result = try(t.test(.y ~ .group.colors, data=df, ...), silent = TRUE)
+  if(!inherits(result, 'try-error')) pv = result$p.value
+  return (data.frame(.ri = df$.ri[1], .ci = df$.ci[1], pv= c(pv)))
+}
+  
+ctx = tercenCtx()
+
+if (length(ctx$colors) < 1) stop("A color factor is required.")
+ 
+ctx %>% 
+  select(.ci, .ri, .y) %>%
+  mutate(.group.colors = do.call(function(...) paste(..., sep='.'), ctx$select(ctx$colors))) %>%
+  group_by(.ci, .ri) %>%
+  do(do.ttest(., 
+              alternative = ctx$op.value('alternative'),
+              mu = as.double(ctx$op.value('mu')), 
+              var.equal = as.logical(ctx$op.value('var.equal')),
+              conf.level = as.double(ctx$op.value('conf.level')))) %>%
+  ctx$addNamespace() %>%
+  ctx$save()
