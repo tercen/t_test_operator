490ae4e27ca9551fee0c2ea464ae24e019bb19d1
agouy <contact@agouy.com>
2022-09-14 15:40:43 +0000

    fixed default settings for test

--- main.R
+++ main.R
@@ -10,7 +10,7 @@ if(length(ctx$colors) < 1) stop("A color factor is required.")
 paired <- ctx$op.value('paired', as.logical, FALSE)
 alternative <- ctx$op.value('alternative', as.character, 'two.sided')
 mu <- ctx$op.value('mu', as.double, 0.0)
-var.equal <- ctx$op.value('var.equal', as.logical, TRUE)
+var.equal <- ctx$op.value('var.equal', as.logical, FALSE)
 conf.level <- ctx$op.value('conf.level', as.double, 0.95)
 p.adjust.method <- ctx$op.value('p.adjust.method', as.character, 'holm')
 
@@ -37,3 +37,4 @@ df <- ctx %>%
   mutate(n1 = as.double(n1), n2 = as.double(n2)) %>%
   ctx$addNamespace() %>%
   ctx$save()
+
